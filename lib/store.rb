4c43cd35112b4b9fdd238c372fa2bb659f5a4eab
AVacAdok <avakuang8@gmail.com>
2022-07-26 18:50:22 +0000

    validations for both models

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {greater_than: 0}
 end
+
+#Ask the user for a store name
+puts "Please input your store name:"
+@store_name = gets.chomp
+
+Store.create(name: @store_name)
+#Display the error messages
+puts @store_name.errors.full_messages
