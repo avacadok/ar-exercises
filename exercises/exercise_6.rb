37263d6c5d1ff4f6584b326da66edf8b56af981a
AVacAdok <avakuang8@gmail.com>
2022-07-26 18:08:30 +0000

    adding employees to store

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+#assign the store id by starting with @store1 or @store2
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Brandon", last_name: "Tu", hourly_rate: 80)
+@store1.employees.create(first_name: "Ava", last_name: "K", hourly_rate: 50)
+@store2.employees.create(first_name: "Snowie", last_name: "K", hourly_rate: 45)
+@store2.employees.create(first_name: "Orange", last_name: "Kitty", hourly_rate: 45)
+@store2.employees.create(first_name: "Maddy", last_name: "K", hourly_rate: 60)
+#@store3 got destroy in previous activities so cannnot be access
+# @store3.employees.create(first_name: "Vicky", last_name: "Tu", hourly_rate: 60)
