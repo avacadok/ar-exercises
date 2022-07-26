d366fc7461e7041909646185c08219b600c0490a
AVacAdok <avakuang8@gmail.com>
2022-07-26 06:25:26 +0000

    Update the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+#assign store to the instance variables
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+#update store name
+@store1.update(name: "Snowie")
